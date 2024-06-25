7cc09275d22fd84302f6d8bf64c6f0820d658141
Paras Puneet Singh <136245940+ParasPuneetSingh@users.noreply.github.com>
2024-06-25 11:34:30 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar"
+import Query from "./components/Query"
+
+const App = () => {
+  return (
+    <>
+    <Navbar /> 
+    <Query />
+    </>
+  )
+
+}
+
+export default App
