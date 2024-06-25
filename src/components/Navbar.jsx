a055b45fc9a8eab8e7fe9c41d9a0f706209c53de
Paras Puneet Singh <136245940+ParasPuneetSingh@users.noreply.github.com>
2024-06-25 11:36:30 +0530

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="whitespace-nowrap w-full h-16 box-content">
+      <p className="-ml-0 inline-block font-montserrat text-6xl text-white pt-0 pl-8">
+        BANK MANAGEMENT SYSTEM
+      </p>
+    </nav>
+  );
+};
+
+export default Navbar;
